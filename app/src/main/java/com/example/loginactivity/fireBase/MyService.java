120f25245b495f6865de5bd0a3bc9552e981b898
noamTzuber <noamtooby@gmail.com>
2022-06-20 17:28:11 +0300

    naom 1

--- app/src/main/java/com/example/loginactivity/fireBase/MyService.java
+++ app/src/main/java/com/example/loginactivity/fireBase/MyService.java
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint;
 import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Intent;
 import android.os.Build;
@@ -13,6 +14,7 @@ import androidx.annotation.NonNull;
 import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationManagerCompat;
 
+import com.example.loginactivity.ContactActivity;
 import com.example.loginactivity.R;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
@@ -24,22 +26,33 @@ public class MyService extends FirebaseMessagingService {
 
     @Override
     public void onMessageReceived(@NonNull RemoteMessage remoteMessage) {
-        if (remoteMessage.getNotification() != null) {
 
-            createNotificationChannel();
+            super.onMessageReceived(remoteMessage);
+            if (remoteMessage.getNotification() != null) {
 
-            NotificationCompat.Builder builder = new NotificationCompat.Builder(this,"1")
-                    .setSmallIcon(R.drawable.profile_pic)
-                    .setContentTitle(remoteMessage.getNotification().getTitle())
-                    .setContentText(remoteMessage.getNotification().getBody())
-                    .setAutoCancel(true)
-                    .setPriority(Notification.PRIORITY_DEFAULT);
+                createNotificationChannel();
 
-            NotificationManagerCompat notificationManagerCompat= NotificationManagerCompat.from(this);
-            notificationManagerCompat.notify(1, builder.build());
+                String n=remoteMessage.getData().get("From");
+                Intent i=new Intent(this, ContactActivity.class);
+                i.putExtra( "idContact",remoteMessage.getData().get("From"));
+                i.putExtra("nameContact",remoteMessage.getData().get("From"));
+                i.putExtra("serverContact","10.0.2.2:1234");
+                PendingIntent pendingIntent=PendingIntent.getActivity(this,0,i,  PendingIntent.FLAG_UPDATE_CURRENT);
 
+//            startActivity(intent);
+
+                NotificationCompat.Builder builder = new NotificationCompat.Builder(this,"1")
+                        .setSmallIcon(R.drawable.profile_pic)
+                        .setContentTitle(remoteMessage.getNotification().getTitle())
+                        .setContentText(remoteMessage.getNotification().getBody())
+                        .setAutoCancel(true)
+                        .setContentIntent(pendingIntent)
+                        .setPriority(Notification.PRIORITY_DEFAULT);
+
+                NotificationManagerCompat notificationManagerCompat= NotificationManagerCompat.from(this);
+                notificationManagerCompat.notify(1, builder.build());
+            }
         }
-    }
 
     private void createNotificationChannel(){
         if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
